e2e83508bfd1cfb09bd6d9aa1a61cfe21c26b7d9
kipli176 <kipli176@gmail.com>
2025-09-29 05:41:29 +0700

    first commi

--- app/config.py
+++ app/config.py
@@ -1,12 +1,12 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    # DB_HOST: str = "postgres"
-    DB_HOST: str = "192.168.137.165"
+    DB_HOST: str = "postgres"
+    # DB_HOST: str = "192.168.137.165"
     DB_USER: str = "kipli_user"
     DB_PASSWORD: str = "kipli_password"
     DB_NAME: str = "bayarinternet"
-    DB_PORT: int = 15432
+    DB_PORT: int = 5432
 
     class Config:
         env_file = ".env"
